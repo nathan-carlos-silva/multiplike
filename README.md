02cd2fa1e7f3c7e179a6d129dbbb86f4d7b449db
Nathan Carlos <n.data.engineer.project@gmail.com>
2025-10-03 00:53:02 -0300

    Atualização

---
+++ README.md
@@ -0,0 +1,32 @@
+
+- `Idade`
+- `Email`
+- `Data_nascimento`
+- `Cidade`
+
+---
+
+## 🎯 Objetivos do Teste
+
+1. **Ler e processar** o arquivo CSV (`dados_brutos.csv`).
+2. **Corrigir problemas de qualidade** nos dados:
+   - Valores inválidos
+   - Dados ausentes
+   - Formatação incorreta
+3. **Aplicar regras de transformação**, como:
+   - Padronização de nomes (capitalizar a coluna `Nome`)
+   - Conversão de tipos de dados
+   - Tratamento de registros inconsistentes
+4. **Criar novas colunas** a partir dos dados existentes:
+   - `Título`
+   - `Dia`
+   - `Mês`
+   - `Ano`
+   - `Dia da semana`
+   - `Estado`
+   - `País`
+5. **Armazenar os dados processados** em um banco de dados **SQLite**.
+6. **Demonstrar boas práticas** de:
+   - Código
+   - Organização
+   - Desempenho
---
+++ src/multiplike/main.py
@@ -0,0 +1,76 @@
+import pandas as pd 
+import sqlite3
+import logging
+import numpy as np
+
+
+
+DB_PATH = 'data/multiplike_db.db'
+CSV_PATH = 'data/teste pratico engenheiro de dados - dados_brutos.csv'
+CHUNCK_SIZE = 2500
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def executar_etl():
+
+    try:
+        with sqlite3.connect(DB_PATH) as conn:
+            with conn: 
+                cursor = conn.cursor()
+
+                cursor.execute("""
+                    DROP TABLE IF EXISTS usuarios_silver                    
+                """)
+                
+                '''cursor.execute("""
+                    CREATE TABLE usuarios_silver (
+                        id INTEGER PRIMARY KEY,
+                        nome TEXT,     
+                        idade INTEGER,  
+                        email TEXT, 
+                        data_nascimento DATE,                               
+                        cidade as TEXT,
+                        dia_nascimento INTEGER,
+                        mes_nascimento INTEGER,                              
+                        ano_nascimento INTEGER
+                    )
+                """)'''
+
+                for i, chunck in enumerate(pd.read_csv(CSV_PATH, encoding='utf-8', sep=',', chunksize=CHUNCK_SIZE)):
+
+                    chunck.columns = (
+                        chunck.columns
+                        .str.strip()
+                        .str.lower()
+                        .str.replace(" ","_",regex=True)
+                        .str.replace(r"[^a-z0-9_]","",regex=True)
+                    )
+
+                    chunck['nome'] = chunck['nome'].str.strip().str.title()
+                    chunck_nome_split = chunck['nome'].str.split('.', n=1, expand=True)
+                    chunck['titulo'] = chunck_nome_split[0].str.strip()
+                    chunck['nome'] = np.where(chunck_nome_split[1].isnull(), chunck_nome_split[0], chunck_nome_split[1])
+                    chunck['nome'] = chunck['nome'].str.strip().str.title()
+                    chunck['cidade'] = chunck['cidade'].str.strip().str.title()
+                    chunck['idade'] = pd.to_numeric(chunck['idade'], errors='coerce')
+                    chunck['idade'] = chunck['idade'].fillna(0).astype(int)
+                    chunck['data_nascimento'] = pd.to_datetime(chunck['data_nascimento'], errors='coerce')
+                    chunck['dia_nascimento'] = chunck['data_nascimento'].dt.day
+                    chunck['mes_nascimento'] = chunck['data_nascimento'].dt.month
+                    chunck['ano_nascimento'] = chunck['data_nascimento'].dt.year
+                    chunck = chunck.drop_duplicates(subset = ['id'])
+                    chunck = chunck.dropna(subset = ['id'])
+
+
+                    chunck.to_sql('usuarios_silver', conn, if_exists ='append', index=False) 
+
+    except Exception as e:
+        logging.exception(f"Erro detectado = {e}")
+        raise
+
+if __name__== '__main__':
+    executar_etl()
